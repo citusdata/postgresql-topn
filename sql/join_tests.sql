457bd7bfadac67cc8996e053a1a815ec9682e3db
Philip Dubé <gunkmute@gmail.com>
2020-09-29 17:17:27 +0000

    Try to come up with a single join_tests output

--- sql/join_tests.sql
+++ sql/join_tests.sql
@@ -1,13 +1,13 @@
 -- Onder dreamed about these queries crashing
-SELECT
-    (topn(topn_add_agg(customer_id), 10)).*, (topn(topn_add_agg(product_id), 10)).*
-FROM
-    customer_reviews
-ORDER BY
-    2 DESC, 4 DESC, 1, 3;
+WITH cte (customer_item, customer_freq, product_item, product_freq) AS (
+	SELECT
+		(topn(topn_add_agg(customer_id), 10)).*, (topn(topn_add_agg(product_id), 10)).*
+	FROM
+		customer_reviews
+) SELECT customer_item, product_item FROM cte ORDER BY floor(customer_freq/10) desc, floor(product_freq/10) desc, customer_item, product_item;
 
 SELECT
-    *
+    customers.product_category, customers.item, products.item
 FROM
     (SELECT
         product_category, (topn(topn_add_agg(customer_id), 10)).*
@@ -27,29 +27,31 @@ JOIN
 ON
     (customers.product_category = products.product_category)
 ORDER BY
-    3 DESC,6 DESC, 1, 2, 4, 5
+    floor(customers.frequency/10) DESC, floor(products.frequency/10) DESC, 1, 2, 3
 LIMIT
     20;
 
+WITH cte (item, freq) AS (
 SELECT
-    (topn(topn_union(topn_union_agg(customers.topn_add_agg), topn_union_agg(products.topn_add_agg)), 15)).* 
-FROM 
-    (SELECT 
-        product_category, topn_add_agg(customer_id) 
-    FROM 
-        customer_reviews 
-    GROUP BY 
+    (topn(topn_union(topn_union_agg(customers.topn_add_agg), topn_union_agg(products.topn_add_agg)), 15)).*
+FROM
+    (SELECT
+        product_category, topn_add_agg(customer_id)
+    FROM
+        customer_reviews
+    GROUP BY
         1
-    ) customers 
-JOIN 
-    (SELECT 
-        product_category, topn_add_agg(product_id) 
-    FROM 
-        customer_reviews 
-    GROUP BY 
+    ) customers
+JOIN
+    (SELECT
+        product_category, topn_add_agg(product_id)
+    FROM
+        customer_reviews
+    GROUP BY
         1
-    ) products 
-ON true 
-ORDER BY 
+    ) products
+ON true)
+SELECT item, floor(freq/10) FROM cte
+ORDER BY
     2 DESC, 1
 LIMIT 5;
